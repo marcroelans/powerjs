19d971e0c560af0e872c951cb674cb06c390dc5a
Jan-Markus Langer <hallo@janmarkuslanger.de>
2018-06-16 23:23:59 +0200

    class Fix
    ELement did not add a class

--- src/utils/is.js
+++ src/utils/is.js
@@ -71,4 +71,9 @@ export const isEvent = expectedEvent => (expectedEvent.startsWith('on')
   * @param {HTMLElement} element
   * @param {String} attribute
   */
-export const isElementAttribute = (element, attribute) => attribute in element;
+export const isElementAttribute = (element, attribute) => {
+  return (
+    attribute in element ||
+    attribute === 'class'
+  ) ;
+};
