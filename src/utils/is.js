c224aefd7e6fde284bf114772ce3898fa836221d
Kieran Boyle <norcal.kieran@gmail.com>
2018-09-15 19:59:31 -0700

    use toString() instead of typeof to detect object class;

--- src/utils/is.js
+++ src/utils/is.js
@@ -1,89 +1,83 @@
-/**
-  * validate an Array
-  *
-  * @param {*} expectedArray
-  *
-  * @returns {Boolean}
-  */
-export const isArray = expectedArray => Array.isArray(expectedArray);
 
 /**
-  * validate a Boolean
-  *
-  * @param {*} expectedBoolean
-  *
-  * @returns {Boolean}
-  */
-export const isBoolean = expectedBoolean => typeof expectedBoolean === 'boolean';
+ * class2type dictionary
+ * @type {Object}
+ */
+const class2type = {};
 
 /**
-  * validate null
-  *
-  * @param {*} expectedNull
-  *
-  * @return {Boolean}
-  */
-export const isNull = expectedNull => expectedNull === null;
+ * Returns the internal JavaScript [Class]] of an object
+ * @param  {Object} obj Object to check the class property of
+ * @return {String}     Only the class property of the object
+ */
+const typeOf = obj => (obj === null ? String(obj) : class2type[{}.toString.call(obj)] || 'object');
 
 /**
-  * validate a String
-  *
-  * @param {*} expectedString
-  *
-  * @returns {Boolean}
-  */
-export const isString = expectedString => typeof expectedString === 'string';
+ * Object containing our "is" methods
+ * @type {Object}
+ */
+const methods = {};
 
 /**
-  * validate an object
-  *
-  * @param {*} expectedObject
-  *
-  * @returns {Boolean}
-  */
-export const isObject = expectedObject => (typeof expectedObject === 'object'
-  && !isNull(expectedObject)
-  && !isArray(expectedObject)
-);
+ * Creates type checks methods: isArray(), isBoolean(), isFunction(), etc...
+ * @param  {Object}  obj Object to check the class property of
+ * @return {Boolean}     Returns true if the passed object passes the type check, else false
+ */
+['Array', 'Boolean', 'Date', 'Error', 'Function', 'Null', 'Number', 'Object', 'RegExp', 'String', 'Undefined'].forEach((type) => {
+  const lcase = type.toLowerCase();
+  // populate class2type object with type
+  class2type[`[object ${type}]`] = lcase;
+  // create isType method
+  methods[`is${type}`] = obj => typeOf(obj) === lcase;
+});
 
-
-/**
-  * validate HTML Element
-  *
-  * @param {*} expectedHtml
-  *
-  * @returns {Boolean}
-  */
-export const isHtml = expectedHtml => expectedHtml instanceof Element;
-
-/**
-  * validate function
-  *
-  * @param {*} expectedFunction
-  *
-  * @returns {Boolean}
-  */
-export const isFunction = expectedFunction => typeof expectedFunction === 'function';
+export const {
+  isArray,
+  isBoolean,
+  isError,
+  isFunction,
+  isNull,
+  isNumber,
+  isObject,
+  isRegExp,
+  isString,
+  isUndefined,
+} = methods;
 
 /**
-  * validate event
-  *
-  * @param {*} expectedEvent
-  *
-  * @returns {Boolean}
-  */
-export const isEvent = expectedEvent => (expectedEvent.startsWith('on')
-  ? expectedEvent
-  : `on${expectedEvent}`) in window;
+ * Determines whether the passed object is the window object
+ * @param  {Object}  obj Object to check
+ * @return {Boolean}     Returns true if the passed object is window, else false
+ */
+export const isWindow = obj => obj != null && obj === obj.window;
 
 /**
-  * validate Element Attribte
-  *
-  * @param {HTMLElement} element
-  * @param {String} attribute
-  */
+ * Determines whether the passed object is the document
+ * @param  {Object}  obj Object to check
+ * @return {Boolean}     Returns true if the passed object is the document, else false
+ */
+export const isDocument = obj => obj != null && obj.nodeType === obj.DOCUMENT_NODE;
+
+/**
+ * Determines whether the passed object is a valid HTML Element
+ * @param  {Object}  obj Object to check
+ * @return {Boolean}     Returns true if the passed object is an element, else false
+ */
+export const isHtml = obj => obj instanceof Element;
+
+/**
+ * Determines whether the passed string is a valid event
+ * @param  {String}  event String containing the event
+ * @return {Boolean}       Returns true if the passed string is an event, else false
+ */
+export const isEvent = event => (event.startsWith('on') ? event : `on${event}`) in window;
+
+/**
+ * Determines whether the passed object is a valid element attribte
+ * @param  {HTMLElement} element   DOM Element to check the property against
+ * @param  {String}      attribute String containing the property name to lookup
+ * @return {Boolean}               Returns true if the passed attribute exists inside the element
+ */
 export const isElementAttribute = (element, attribute) => (
-  attribute in element
-  || attribute === 'class'
-  || attribute.startsWith('data-', 0)
+  attribute in element || attribute === 'class' || attribute.startsWith('data-', 0)
 );
